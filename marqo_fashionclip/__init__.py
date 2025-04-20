1325f7a3ba67f9cc8fd3de82e23b242f639ff9da
Heidi1213 <hae081128@sookmyung.ac.kr>
2025-04-20 20:42:09 +0900

    [ADD] marqo_fashionclip

---
+++ marqo_fashionclip/__init__.py
@@ -0,0 +1,37 @@
+from model_utils import ImageEmbeddingModel
+import open_clip
+import torch
+from tqdm import tqdm
+
+
+class MarqoFashionCLIP(ImageEmbeddingModel):
+    def __init__(self, model_name, cfg):
+        super().__init__(model_name, cfg)
+        model, _, preprocess = open_clip.create_model_and_transforms(
+            'hf-hub:Marqo/marqo-fashionCLIP', device=self.device)
+        
+        model.to(self.device)
+        model.eval()
+
+        self._model = model
+        self._preprocess = preprocess  
+
+    @property
+    def model(self):
+        return self._model
+
+    def forward(self, pil_images):
+        processed_images = []
+        with tqdm(total=len(pil_images), desc="Index examples") as progress:
+            for img in pil_images:
+                processed_img = self._preprocess(img)
+                processed_images.append(processed_img)
+                progress.update(1)
+                
+        iimages = torch.stack(processed_images).to(self.device)
+        image_features = self.model.encode_image(iimages).detach().cpu()
+        norm_factors = image_features.norm(dim=-1, keepdim=True)
+        image_features /= norm_factors
+        batch_embeddings_ndarray = image_features.numpy()
+
+        return batch_embeddings_ndarray
