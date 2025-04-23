4e80ace90b88273adc87e3736d1d9b125602d283
sorikiki98 <sorikiki98@naver.com>
2025-04-23 15:17:07 +0900

    [ADD] unicom

---
+++ unicom/__init__.py
@@ -0,0 +1,35 @@
+from .model import load, available_models
+from model_utils import ImageEmbeddingModel
+from tqdm import tqdm
+import torch
+
+
+class Unicom(ImageEmbeddingModel):
+    def __init__(self, model_name, cfg):
+        super().__init__(model_name, cfg)
+        unicom_model, preprocess = load("ViT-B/32")
+        unicom_model.to(self.device)
+        unicom_model.eval()
+
+        self._preprocess = preprocess
+        self._model = unicom_model
+
+    @property
+    def model(self):
+        return self._model
+
+    def forward(self, pil_images):
+        processed_images = []
+        with tqdm(total=len(pil_images), desc="Index examples") as progress:
+            for img in pil_images:
+                processed_img = self._preprocess(img)
+                processed_images.append(processed_img)
+                progress.update(1)
+        iimages = torch.stack(processed_images).to(self.device)
+        with torch.no_grad():
+            batch_embeddings = self.model(iimages)
+            batch_embeddings = batch_embeddings / batch_embeddings.norm(dim=-1,
+                                                                        keepdim=True)
+        batch_embeddings_ndarray = batch_embeddings.cpu().numpy()
+
+        return batch_embeddings_ndarray
