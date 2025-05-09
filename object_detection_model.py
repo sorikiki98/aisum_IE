eeb6e14c6142f2b5e61891607eaad3c41b7896a9
sorikiki98 <sorikiki98@naver.com>
2025-05-09 17:23:15 +0900

    [MOD] object detection model is_indexing

--- object_detection_model.py
+++ object_detection_model.py
@@ -3,10 +3,11 @@ import torch.nn as nn
 
 
 class ObjectDetectionModel(nn.Module, ABC):
-    def __init__(self, model_name, cfg):
+    def __init__(self, model_name, cfg, indexing=False):
         super().__init__()
         self._name = model_name
         self.model_cfg = cfg["model"][model_name]
+        self._indexing = indexing
 
     @property
     @abstractmethod
